34ee77b4ed63986434ae1f11725d43e165283d8c
adwinscottrussell-pixel <adwinscottrussell@gmail.com>
2026-05-31 13:42:43 +0200

    chore: remove remaining patch files and bak files

--- backend/patch_lp_url.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs');
-const p=require('path').join(__dirname,'src/services/passService.js');
-let f=fs.readFileSync(p,'utf8').replace(/\r\n/g,'\n');
-const OLD="WALLET_CONFIG.webServiceUrl.replace('/api','')";
-const NEW="'https://qraivy.com'";
-if(f.includes(OLD)){f=f.replace(OLD,NEW);console.log('done');}
-else console.log('FAIL');
-fs.writeFileSync(p,f,'utf8');
