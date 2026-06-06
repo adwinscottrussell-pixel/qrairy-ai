8ee60d044be81fd6221ad9d0f64dfa69fbe29b19
adwinscottrussell-pixel <adwinscottrussell@gmail.com>
2026-06-06 17:35:04 +0200

    fix: restore simple template strings for bizName and slug in rendered JS

--- backend/src/controllers/lpController.js
+++ backend/src/controllers/lpController.js
@@ -774,8 +774,8 @@ ${sectionsHTML}`;
 (function(){
   var playing = false;
   var aiActivated = false;
-  var bizName = `${JSON.stringify(bizName)}`;
-  var slug = `${JSON.stringify(slug)}`;
+  var bizName = '${bizName}';
+  var slug = '${slug}';
 
   var voiceBtn  = document.getElementById('voiceBtn');
   var waveform  = document.getElementById('waveform');
